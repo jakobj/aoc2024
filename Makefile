c065b2294b10c6ec5c5199353a4632797928e6fd
Jakob Jordan <jakobjordan@posteo.de>
2024-12-01 21:48:56 -0500

    Solve day 1 part 2

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 CC=clang -Wall -Werror -std=c23 -fdiagnostics-color
 
+.PHONY: format
+
 src/day01: src/day01.c
 	cd src && $(CC) -o day01 day01.c && ./day01
 
--- src/day01.c
+++ src/day01.c
@@ -18,17 +18,14 @@ size_t read_input(char *fn, int first[], int second[]) {
     fclose(instream);
   } else {
     fprintf(stderr, "Could not open file \"%s\"\n", fn);
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
   return i;
 }
 
 int compare(const void *a, const void *b) { return *(int *)a - *(int *)b; }
 
-int main() {
-  int first[array_max] = {};
-  int second[array_max] = {};
-  const size_t n_elements = read_input("../inputs/01.txt", first, second);
+void part1(int first[], int second[], int n_elements) {
   qsort(first, n_elements, sizeof(int), compare);
   qsort(second, n_elements, sizeof(int), compare);
   int total_distance = 0;
@@ -36,5 +33,27 @@ int main() {
     total_distance += abs(first[i] - second[i]);
   }
   printf("The total distance between the lists is %d.\n", total_distance);
+}
+
+void part2(int first[], int second[], int n_elements) {
+  int similarity_score = 0;
+  for (size_t i = 0; i < n_elements; ++i) {
+    int count = 0;
+    for (size_t j = 0; j < n_elements; ++j) {
+      if (first[i] == second[j]) {
+        ++count;
+      }
+    }
+    similarity_score += first[i] * count;
+  }
+  printf("Their similarity score is %d.\n", similarity_score);
+}
+
+int main() {
+  int first[array_max] = {};
+  int second[array_max] = {};
+  const size_t n_elements = read_input("../inputs/01.txt", first, second);
+  part1(first, second, n_elements);
+  part2(first, second, n_elements);
   return EXIT_SUCCESS;
 }
